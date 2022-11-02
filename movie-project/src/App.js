63a46e8d8f5c29f2ad7b3d3d68bc8c8e9f9035d4
jungeunpark <jung2uncan@gmail.com>
2022-11-02 16:54:39 +0900

    [EDIT] props & state를 이용해서 헤더 문구 넣기

--- movie-project/src/App.js
+++ movie-project/src/App.js
@@ -1,52 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Header from './header';
+
 class App extends Component {
-
-  //App컴포넌트에 생성한 state는, App컴포넌트에서만 업데이트 할 수 있음.
   state = {
-    count: 0
+    headerMessage: '영화 소개 프로젝트'
   };
-
-  countUp() {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
-
-  countDown(){
-    this.setState({
-      count: this.state.count - 1
-    });
-  }
 
   render() {
     return(
       <div calssName='App'>
-        {/* <div>
-          <span>Current Count : {this.state.count}</span>
-        </div> */}
-        <PropsTestClass count={this.state.count}/>
-        <div>
-          <button onClick={this.countUp.bind(this)}>Count Up!</button>
-          <button onClick={this.countDown.bind(this)}>Count Down!</button>
-        </div>
+        <Header headerMessage={this.state.headerMessage}/>
+
+        <div>Main Content</div>
       </div>
     )
-  }
-}
-
-//props
-class PropsTestClass extends Component {
-  render() {
-    return (
-      <div>
-        <div>
-          <span> Hello I am TestClass </span>
-        </div>
-        <div> 받은 Props의 데이터는 {this.props.count} 입니다. </div>
-      </div>
-    );
   }
 }
 
--- movie-project/src/header.js
+++ movie-project/src/header.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Header extends Component {
     render() {
-        return <div>Hello I am header Component!</div>
+        return <div>{this.props.headerMessage}</div>
     }
 }
 
