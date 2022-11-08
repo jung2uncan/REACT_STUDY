14d36edf793917421d0921aef04521a7e99bbff5
jungeunpark <jung2uncan@gmail.com>
2022-11-08 17:20:29 +0900

    [EDIT] Link 태그를 이용한 페이지 이동

--- client-side-rendering/src/App.js
+++ client-side-rendering/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Switch, Route, Routes } from 'react-router-dom';
+// import { Switch, Route, Routes, Link, NavLink } from 'react-router-dom';
+import { Route, Routes, NavLink } from 'react-router-dom';
 import './App.css';
 
 import Home from './components/home';
@@ -9,9 +10,44 @@ import Main from './components/main';
 
 class App extends Component {
   render () {
+    const activeStyle = {
+      backgroud: 'black',
+      color: 'white',
+    };
+
     return (
       <div className='App'>
         클라이언트 사이드 렌더링
+        {/* Link 태그를 이용한 페이지 이동 */}
+        <div>
+          <ul className="link-list">
+            <li>
+              {/* 다른 태그와 마찬가지로 className을 넣을 수 있음 */}
+              {/* 이동하려 하는 URL을 to라는 Attribute로 표현하면 됨 */}
+              {/* <Link className="link" to="/">
+                메인 페이지로 이동
+              </Link> */}
+              <NavLink activeStyle={activeStyle} className="link" to="/">
+                메인 페이지로 이동
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeStyle={activeStyle} className="link" to="/home">
+                홈 페이지로 이동
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeStyle={activeStyle} className="link" to="/first">
+                First 페이지로 이동
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeStyle={activeStyle} className="link" to="/second">
+                Second 페이지로 이동
+              </NavLink>
+            </li>
+          </ul>
+        </div>
         {/* react-router-dom에서 제공하는 태그. */}
         {/* path 부분에 url 경로를 넣고, component의 to의 경로에서 보여주고 싶은 컴포넌트를 넣어주면 됨 */}
         <Routes>
