45fba1f7e4a90b42b8573c0e4d5dfcd14cbd4a93
Jungeun Park <jung2uncan@gmail.com>
2022-12-26 14:59:12 +0900

    [ADD] React state

--- LiveCoding/react-app-01/src/App.js
+++ LiveCoding/react-app-01/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 
 function Header(props) {
   console.log("props", props);
@@ -32,7 +33,7 @@ function Nav(props) {
           href={"/read/" + t.id}
           onClick={(event) => {
             event.preventDefault();
-            props.onChangeMode(event.target.id);
+            props.onChangeMode(Number(event.target.id));
           }}
         >
           {t.title}
@@ -58,30 +59,59 @@ function Article(props) {
 }
 
 function App() {
+  // useState 의 인자는 데이터의 초기값.
+  // useState의 반환 값 [데이터 상태 값, 데이터 변경 set함수]
+  const [mode, setMode] = useState("WELCOME");
+  const [id, setId] = useState(null);
+
   const topics = [
     { id: 1, title: "html", body: "html is ..." },
     { id: 2, title: "css", body: "css is ..." },
     { id: 3, title: "javascript", body: "javascript is ..." },
   ];
 
+  let content = null;
+  if (mode === "WELCOME") {
+    content = <Article title="Welcome" body="Hello, WEB!"></Article>;
+  } else if (mode === "READ") {
+    let title = null;
+    let body = null;
+    for (let i = 0; i < topics.length; i++) {
+      if (topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>;
+  }
+
   return (
     <div className="App">
       <Header
         title="WEB"
         onChangeMode={function () {
-          alert("Header");
+          setMode("WELCOME");
         }}
       ></Header>
       <Nav
         topics={topics}
-        onChangeMode={(id) => {
-          alert(id);
+        onChangeMode={(_id) => {
+          setMode("READ");
+          setId(_id);
         }}
       ></Nav>
       {/* 배열을 props로 넘겨줄 때, { 배열명 } 포맷으로 넘김  */}
-      <Article title="Welcome" body="Hello, WEB!"></Article>
+      {content}
     </div>
   );
 }
 
 export default App;
+
+/**
+ * Props VS State
+ *  Props: 컴포넌트를 사용하는 외부자를 위한 데이터
+ *  State : 컴포넌트를 만드는 내부자를 위한 데이터
+ *
+ *  공통점은 값이 변경되면 새로운 retrun값을 만들어 UI를 변경함.
+ */
