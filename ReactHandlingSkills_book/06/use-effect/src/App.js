4c38e7da6ea773424985c358efb51b9edcf8b6cc
Jungeun Park <jung2uncan@gmail.com>
2022-12-27 15:57:57 +0900

    [ADD] useEffect, useMemo, useCallback 예제 실습

---
+++ ReactHandlingSkills_book/06/use-effect/src/App.js
@@ -0,0 +1,105 @@
+import React, { useRef, useState, useMemo, useCallback } from "react";
+import CreateUser from "./CreateUser";
+import UserList from "./UserList";
+
+function countActiveUsers(users) {
+  console.log("활성 사용자 수를 세는 중...");
+  return users.filter((user) => user.active).length;
+}
+
+function App() {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+  });
+
+  const { username, email } = inputs;
+
+  const onChange = useCallback(
+    (e) => {
+      const { name, value } = e.target;
+      setInputs({
+        ...inputs,
+        [name]: value,
+      });
+    },
+    [inputs]
+  );
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: "jungeun",
+      email: "jungeun123@mail.com",
+      active: true,
+    },
+    {
+      id: 2,
+      username: "tester",
+      email: "tester@example.com",
+      active: true,
+    },
+    {
+      id: 3,
+      username: "테스트",
+      email: "테스트@example.com",
+      active: false,
+    },
+  ]);
+
+  const nextId = useRef(4); //useRef() 를 사용 할 때 파라미터를 넣어주면, 이 값이 .current 값의 기본값이 됨.
+  //이 값을 수정 할때에는 .current 값을 수정하면 되고 조회 할 때에는 .current 를 조회하면 됨.
+  const onCreate = useCallback(() => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    //setUsers([...users, user]); //기존 userList에 신규 user 추가
+    setUsers(users.concat(user)); //concat 함수는 기존의 배열을 수정하지 않고, 새로운 원소가 추가된 새로운 배열을 만들어 줌.
+
+    setInputs({
+      username: "",
+      email: "",
+    });
+
+    nextId.current += 1;
+  }, [users, username, email]);
+
+  const onRemove = useCallback(
+    (id) => {
+      // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+      // = user.id 가 id 인 것을 제거함
+      setUsers(users.filter((user) => user.id !== id));
+    },
+    [users]
+  );
+
+  const onToggle = useCallback(
+    (id) => {
+      setUsers(
+        users.map((user) =>
+          user.id === id ? { ...user, active: !user.active } : user
+        )
+      );
+    },
+    [users]
+  );
+
+  const count = useMemo(() => countActiveUsers(users), [users]);
+
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>활성 사용자 수 : {count}</div>
+    </>
+  );
+}
+
+export default App;
---
+++ ReactHandlingSkills_book/06/use-effect/src/UserList.js
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react";
+
+function User({ user, onRemove, onToggle }) {
+  //   useEffect(() => {
+  //     console.log("user 값이 설정됨.");
+  //     console.log(user);
+  //     return () => {
+  //       console.log("user가 바뀌기 전..");
+  //       console.log(user);
+  //     };
+  //   }, [user]);
+
+  // deps 파라미터 생략하기
+  useEffect(() => {
+    console.log(user);
+  });
+
+  return (
+    <div>
+      <b
+        style={{
+          cursor: "pointer",
+          color: user.active ? "green" : "black",
+        }}
+        onClick={() => onToggle(user.id)}
+      >
+        {user.username}
+      </b>
+      &nbsp;
+      <span>({user.email})</span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
+    </div>
+  );
+}
+
+function UserList({ users, onRemove, onToggle }) {
+  return (
+    <div>
+      {users.map((user) => (
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default UserList;
