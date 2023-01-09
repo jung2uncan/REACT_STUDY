07b7ef7b817343ca6d14f0714965eab791fb2e34
Jungeun Park <jung2uncan@gmail.com>
2023-01-09 15:44:19 +0900

    [EDIT] state안에 고정된 값이 존재하는 경우 예시

--- ReactHandlingSkills_book/09/class-component/src/Counter.js
+++ ReactHandlingSkills_book/09/class-component/src/Counter.js
@@ -1,52 +1,24 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  //.this에 컴포넌트 인스턴스 매핑하려면?
-
-  // 방법 1) 클래스의 생성자 메서드 constructor 에서 bind 작업
-  //   constructor(props) {
-  //     super(props);
-  //     this.handleIncrease = this.handleIncrease.bind(this);
-  //     this.handleDecrease = this.handleDecrease.bind(this);
-  //   }
-
-  //   // 클래스 내부에 종속된 함수를 "메소드"라고 함.
-  //   handleIncrease() {
-  //     console.log("INCREASE!");
-  //     console.log(this);
-  //   }
-
-  //   handleDecrease() {
-  //     console.log("DECREASE!");
-  //   }
-
-  // 방법 2) 커스텀 메서드를 선언 할 때 화살표 함수 문법을 사용하기
-  //   handleIncrease = () => {
-  //     console.log("INCREASE!");
-  //     console.log(this);
-  //   };
-
-  //   handleDecrease = () => {
-  //     console.log("DECREASE!");
-  //   };
-
-  // 방법 3) 상태(state) 선언하기
-  constructor(props) {
-    super(props);
-    // 클래스형 컴포넌트의 state는 무조건 객체형태여야 함.
-    //render 메서드에서 state 를 조회하려면 this.state로 조회함
-    this.state = {
-      counter: 0,
-    };
-  }
+  state = {
+    counter: 0,
+    fixed: 1,
+  };
 
   handleIncrease = () => {
     console.log("INCREASE!");
     console.log(this);
+    this.setState({
+      counter: this.state.counter + 1,
+    });
   };
 
   handleDecrease = () => {
     console.log("DECREASE!");
+    this.setState({
+      counter: this.state.counter - 1,
+    });
   };
 
   render() {
@@ -55,6 +27,8 @@ class Counter extends Component {
         <h1>{this.state.counter}</h1>
         <button onClick={this.handleIncrease}>+1</button>
         <button onClick={this.handleDecrease}>-1</button>
+        <p>고정된 값 : {this.state.fixed}</p>{" "}
+        {/* this.setState 를 할 때 파라미터에 넣는 객체에 fixed 값을 넣어주지 않아도 값이 유지됨.*/}
       </div>
     );
   }
