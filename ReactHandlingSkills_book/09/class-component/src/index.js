975900b98aa2323d7084ae53596635a65aa89f6f
Jungeun Park <jung2uncan@gmail.com>
2023-01-09 17:00:53 +0900

    [ADD] 라이프 사이클 메소드 내용 정리

--- ReactHandlingSkills_book/09/class-component/src/index.js
+++ ReactHandlingSkills_book/09/class-component/src/index.js
@@ -1,17 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-import reportWebVitals from "./reportWebVitals";
-import Counter from "./Counter";
+import "./styles.css";
+import LifeCycleSample from "./LifeCycleSample";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Counter />
-  </React.StrictMode>
-);
+// 랜덤 색상을 생성합니다
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  const [color, setColor] = useState("#000000");
+  const [visible, setVisible] = useState(true);
+
+  const onClick = () => {
+    setColor(getRandomColor());
+  };
+
+  const onToggle = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <>
+      <button onClick={onClick}>랜덤 색상</button>
+      <button onClick={onToggle}>토글</button>
+      {visible && <LifeCycleSample color={color} />}
+    </>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
