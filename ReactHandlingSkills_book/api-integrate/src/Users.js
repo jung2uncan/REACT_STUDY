bc3e699a83727b15b48504f849736ca7a0369194
Jungeun Park <jung2uncan@gmail.com>
2023-01-20 16:35:56 +0900

    [ADD] 버튼으로 API 호출하기

--- ReactHandlingSkills_book/api-integrate/src/Users.js
+++ ReactHandlingSkills_book/api-integrate/src/Users.js
@@ -9,25 +9,25 @@ function Users() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        //요청이 시작할 때는 error와 users를 초기화하고,
-        setError(null);
-        setUsers(null);
-        //loading 상태를 true로 바꿔줌.
-        setLoading(true);
-        const response = await axios.get(
-          "https://jsonplaceholder.typicode.com/users"
-          //"https://jsonplaceholder.typicode.com/users/showmeerror"
-        );
-        setUsers(response.data);
-      } catch (e) {
-        setError(e);
-      }
-      setLoading(false);
-    };
+  const fetchUsers = async () => {
+    try {
+      //요청이 시작할 때는 error와 users를 초기화하고,
+      setError(null);
+      setUsers(null);
+      //loading 상태를 true로 바꿔줌.
+      setLoading(true);
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+        //"https://jsonplaceholder.typicode.com/users/showmeerror"
+      );
+      setUsers(response.data);
+    } catch (e) {
+      setError(e);
+    }
+    setLoading(false);
+  };
 
+  useEffect(() => {
     fetchUsers();
   }, []);
 
@@ -35,13 +35,16 @@ function Users() {
   if (error) return <div>에러가 발생했습니다</div>;
   if (!users) return null;
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>
-          {user.username} ({user.name})
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.username} ({user.name})
+          </li>
+        ))}
+      </ul>
+      <button onClick={fetchUsers}>다시 불러오기</button>
+    </>
   );
 }
 
