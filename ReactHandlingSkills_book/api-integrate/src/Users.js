20abe82234ade7fc34e39a8426a9e7d9de5ad052
Jungeun Park <jung2uncan@gmail.com>
2023-01-25 14:18:23 +0900

    [ADD] useAsync 커스텀 Hook 만들어서 사용하기

--- ReactHandlingSkills_book/api-integrate/src/Users.js
+++ ReactHandlingSkills_book/api-integrate/src/Users.js
@@ -1,39 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import useAsync from "./useAsync";
 
-//useState 와 useEffect로 데이터 로딩하기
-//1. useState 를 사용하여 요청 상태를 관리하고,
-//2. useEffect를 사용하여 컴포넌트가 렌더링되는 시점에 요청을 시작하는 작업 진행
+/*
+ useAsync에서는 Promise의 결과를 바로 data에 담기 때문에,
+ 요청을 한 이후, response에서 data를 추출하여 반환하는 함수를 따로 만듦.
+*/
+async function getUsers() {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+  return response.data;
+}
+
 function Users() {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [state, refetch] = useAsync(getUsers, []);
 
-  const fetchUsers = async () => {
-    try {
-      //요청이 시작할 때는 error와 users를 초기화하고,
-      setError(null);
-      setUsers(null);
-      //loading 상태를 true로 바꿔줌.
-      setLoading(true);
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/users"
-        //"https://jsonplaceholder.typicode.com/users/showmeerror"
-      );
-      setUsers(response.data);
-    } catch (e) {
-      setError(e);
-    }
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+  const { loading, data: users, error } = state; //state.data를 users 키워드로 조회
 
   if (loading) return <div>로딩중..</div>;
-  if (error) return <div>에러가 발생했습니다</div>;
   if (!users) return null;
+  if (error) return <div>에러가 발생했습니다..</div>;
+
   return (
     <>
       <ul>
@@ -43,7 +31,7 @@ function Users() {
           </li>
         ))}
       </ul>
-      <button onClick={fetchUsers}>다시 불러오기</button>
+      <button onClick={refetch}>다시 불러오기</button>
     </>
   );
 }
---
+++ ReactHandlingSkills_book/api-integrate/src/useAsync.js
@@ -0,0 +1,56 @@
+import { useReducer, useEffect } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "LOADING":
+      return {
+        loading: true,
+        data: null,
+        error: null,
+      };
+    case "SUCCESS":
+      return {
+        loading: false,
+        data: action.data,
+        error: null,
+      };
+    case "ERROR":
+      return {
+        loading: false,
+        data: null,
+        error: action.error,
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
+//첫번째 파라미터는 API요청을 시작하는 함수
+//두번째 파라미터는 deps값은 해당 함수 안에서 사용하는 useEffect의 deps로 설정
+function useAsync(callback, deps = []) {
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: null,
+  });
+
+  const fetchData = async () => {
+    dispatch({ type: "LOADING" });
+    try {
+      const data = await callback();
+      dispatch({ type: "SUCCESS", data });
+    } catch (e) {
+      dispatch({ type: "ERROR", error: e });
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+    // eslint 설정을 다음 줄에서만 비활성화
+    // eslint-disable-next-line
+  }, deps);
+
+  return [state, fetchData];
+}
+
+export default useAsync;
