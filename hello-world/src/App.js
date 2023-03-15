953382ecd20f03ba21c8ad935d75f1ab4f03e7ee
Jungeun Park <jung2uncan@gmail.com>
2023-03-15 17:59:57 +0900

    Ref 사용해보기

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,23 +1,46 @@
-import React from "react";
+import { Component } from "react";
 import MyComponent from "./MyComponent";
 import CounterClass from "./CounterClass";
 import Say from "./Say";
 import EventPractice from "./EventPractice";
+import ValidationSample from "./ValidationSample";
+import ScrollBox from "./ScrollBox";
 
-const App = () => {
-  return (
-    <>
-      <MyComponent name="리액트트" favoriteNumber={3}>
-        리액트
-      </MyComponent>
-      <br />
-      <CounterClass></CounterClass>
-      <br />
-      <Say></Say>
-      <br />
-      <EventPractice />
-    </>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div style={{ width: "500px", float: "left" }}>
+          <h4>==============실습1==============</h4>
+          <MyComponent name="리액트트" favoriteNumber={3}>
+            리액트
+          </MyComponent>
+
+          <h4>==============실습2==============</h4>
+          <CounterClass></CounterClass>
+
+          <h4>==============실습3==============</h4>
+          <Say></Say>
+
+          <h4>==============실습4==============</h4>
+          <EventPractice />
+
+          <h4>==============실습5==============</h4>
+          <ValidationSample />
+        </div>
+
+        <div tyle={{ width: "500px", float: "right" }}>
+          <h4>==============실습6==============</h4>
+          <div>
+            <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+            <button onClick={() => this.scrollBox.scrollToBottom()}>
+              맨 밑으로
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
 
 export default App;
--- hello-world/src/Say.js
+++ hello-world/src/Say.js
@@ -4,6 +4,7 @@ const Say = () => {
   const [message, setMessage] = useState("");
   const onClickEnter = () => setMessage("안녕하세요!");
   const onClickLeave = () => setMessage("안녕히가세요!");
+  const onClickClear = () => setMessage("");
 
   const [color, setColor] = useState("");
 
@@ -11,6 +12,7 @@ const Say = () => {
     <div>
       <button onClick={onClickEnter}>입장</button>
       <button onClick={onClickLeave}>퇴장</button>
+      <button onClick={onClickClear}>초기화</button>
       <h1 style={{ color }}>{message}</h1>
 
       <button style={{ color: "red" }} onClick={() => setColor("red")}>
