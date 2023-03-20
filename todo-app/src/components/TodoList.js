700e17f4bcb5b27b037e2ae648398cd86f9731f8
Jungeun Park <jung2uncan@gmail.com>
2023-03-20 15:06:15 +0900

    [ADD] Todo App 수정, 삭제, 추가 기능 구현

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,12 +1,19 @@
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoList = () => {
+const TodoList = (props) => {
+  const { todos, onRemove, onToggle } = props;
+
   return (
     <div className="TodoList">
-      <TodoListItem />
-      <TodoListItem />
-      <TodoListItem />
+      {todos.map((todo) => (
+        <TodoListItem
+          todo={todo}
+          key={todo.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+      ))}
     </div>
   );
 };
