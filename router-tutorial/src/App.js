178a000257981d88cc70cdb61862fae530097f5b
Jungeun Park <jung2uncan@gmail.com>
2023-03-22 15:01:33 +0900

    NotFound 페이지 구현 및 조건에 따른 Redirect 설정하기

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -5,6 +5,9 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Layout from "./Layout";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import MyPage from "./pages/MyPage";
 
 const App = () => {
   return (
@@ -13,10 +16,14 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/profiles/:username" element={<Profile />} />
-        <Route path="/articles" element={<Articles />}>
-          <Route path=":id" element={<Article />} />
-        </Route>
       </Route>
+      <Route path="/articles" element={<Articles />}>
+        <Route path=":id" element={<Article />} />
+      </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/mypage" element={<MyPage />} />
+      {/* path="*" 이란? 해당 라우트 상단에 위치하는 라우트들의 규칙을 모두 확인하고 일치하는 라우트가 없다면 해당 라우트를 보여줌 */}
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
---
+++ router-tutorial/src/pages/NotFound.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: 63,
+        position: "absolute",
+        witdh: "100%",
+        height: "100%",
+      }}
+    >
+      404
+    </div>
+  );
+};
+
+export default NotFound;
