ca0810f5a1b74b51e24f4e24a6ba49245cb096e3
Jungeun Park <jung2uncan@gmail.com>
2023-03-23 13:56:20 +0900

    동적 Context 사용하기

--- context-tutorial/src/components/ColorBox.js
+++ context-tutorial/src/components/ColorBox.js
@@ -1,19 +1,30 @@
 import ColorContext from "../contexts/color";
+import { ColorConsumer } from "../contexts/color";
 
 const ColorBox = () => {
   return (
     // Consumer사이에 중괄호를 열어서 그 안에 함수를 넣어주는 패턴을 Function as a child 혹은 Render Props 라고 함.
-    <ColorContext.Consumer>
-      {(value) => (
-        <div
-          style={{
-            width: "64px",
-            height: "64px",
-            background: value.color,
-          }}
-        />
+    <ColorConsumer>
+      {/* 비구조화 할당 */}
+      {({ state }) => (
+        <>
+          <div
+            style={{
+              width: "64px",
+              height: "64px",
+              background: state.color,
+            }}
+          />
+          <div
+            style={{
+              width: "32px",
+              height: "32px",
+              background: state.subcolor,
+            }}
+          />
+        </>
       )}
-    </ColorContext.Consumer>
+    </ColorConsumer>
   );
 };
 
