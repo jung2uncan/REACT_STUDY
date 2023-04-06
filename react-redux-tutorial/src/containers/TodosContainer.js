8518c288d2e868855be0b0000cfbd4f376d0d72c
Jungeun Park <jung2uncan@gmail.com>
2023-04-06 17:39:53 +0900

    Hooks를 사용하여 컨테이너 컴포넌트 만들기

--- react-redux-tutorial/src/containers/TodosContainer.js
+++ react-redux-tutorial/src/containers/TodosContainer.js
@@ -1,7 +1,8 @@
 import { connect, useDispatch, useSelector } from 'react-redux';
 import todos, { changeInput, insert, toggle, remove } from '../modules/todos';
 import Todos from '../components/Todos';
-import { useCallback } from 'react';
+import React, { useCallback } from 'react';
+import useActions from '../lib/useActions';
 
 const TodosContainer = () => {
   const { input, todos } = useSelector(({ todos }) => ({
@@ -9,14 +10,20 @@ const TodosContainer = () => {
     todos: todos.todos,
   }));
 
-  const dispatch = useDispatch();
-  const onChangeInput = useCallback(
-    (input) => dispatch(changeInput(input)),
-    [dispatch],
+  // const dispatch = useDispatch();
+  // const onChangeInput = useCallback(
+  //   (input) => dispatch(changeInput(input)),
+  //   [dispatch],
+  // );
+  // const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
+  // const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
+  // const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
+
+  //useActions 사용하기
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+    [changeInput, insert, toggle, remove],
+    [],
   );
-  const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
-  const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
-  const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
 
   return (
     <Todos
@@ -30,4 +37,12 @@ const TodosContainer = () => {
   );
 };
 
-export default TodosContainer;
+export default React.memo(TodosContainer);
+
+/*
+  connect 함수를 사용하여 컨테이너 컴포넌트를 만들면, 
+  해당 컨테이너 컴포넌트의 부모 컴포넌트가 리렌더링될 때 해당 컨테이너 컴포넌트의 props가 바뀌지 않았다면 리렌더링이 자동으로 방지되어 성능이 최적화 됨.
+
+  useSelector를 사용하여, 리덕스 상태를 조회했을 때는 이 최적화 작업이 자동으로 이뤄지지 않으므로,
+  성능 최적화를 위해서는 React.memo를 컨테이너 컴포넌트에 사용하여 최적화 작업을 해줘야 함.
+*/
